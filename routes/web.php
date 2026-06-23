b6aed987b35832b97b2b6b84507e7cac139ce838
JelmervdMeer <jelmervandermeer02@gmail.com>
2026-06-23 06:59:00 +0200

    opdrachten

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/items', function () {})->name('items.index');
+Route::get('/items/create', function () {})->name('items.create');
+Route::post('/items', function () {})->name('items.store');
+Route::get('/items/{id}', function () {})->name('items.show');
+Route::get('/items/{id}/edit', function () {})->name('items.edit');
+Route::put('/items/{id}', function () {})->name('items.update');
+Route::delete('/items/{item}', [ItemController::class, 'destroy'])
+    ->name('items.destroy');
+
+
+Route::get('/items', [ItemController::class, 'index'])
+    ->name('items.index');
+
+Route::get('/items/create', [ItemController::class, 'create'])
+    ->name('items.create');
+
+Route::redirect('/', '/items');
+
+Route::post('/items', [ItemController::class, 'store'])->name('items.store');
+
+Route::get('/items/{id}/edit', [ItemController::class, 'edit'])->name('items.edit');
+
+Route::put('/items/{item}', [ItemController::class, 'update'])->name('items.update');
+
+
